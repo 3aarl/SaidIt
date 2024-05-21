9733da42786df4e7d37a8a16671ee3a623b724fc
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2024-05-22 02:05:45 +0800

    Updated community , new feature to form and creation of route resource
    Added Fillable fields
    - Created community controller & request
    - Created route resource
    - add feature an error for every every empty input on register form

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
-
+use App\Http\Controllers\Backend\CommunityController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -25,9 +14,6 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -35,4 +21,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::group(['middleware'=>['auth','verified']],function(){
+
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+
+    Route::resource(name:'/communitie', controller:CommunityController::class);
+});
 require __DIR__.'/auth.php';
