24f51c4c87df3cc5050d5129a3345dc0e723c7e9
3aarl <king1527emmanuel@gmail.com>
2024-05-29 20:27:50 +0800

    Small Fix
    uses link instead of <a>

--- resources/js/Components/PostVote.vue
+++ resources/js/Components/PostVote.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-2 flex w-12 flex-col p-2">
-    <a href="#" class="text-gray-400 hover:text-blue-500">
+    <Link href="#" class="text-gray-400 hover:text-blue-500">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -15,9 +15,9 @@
           d="M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"
         />
       </svg>
-    </a>
+    </Link>
     <div>4k</div>
-    <a href="#" class="text-gray-400 hover:text-red-500">
+    <Link href="#" class="text-gray-400 hover:text-red-500">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -32,8 +32,10 @@
           d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3"
         />
       </svg>
-    </a>
+    </Link>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
